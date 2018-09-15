f2f88230113a731649d41088725aaf0e5b58714e
GNouchi <gnouchi7@gmail.com>
2018-09-15 15:30:26 -0700

    .

---
+++ src/main/java/com/coding/StudentRoster1/StudentRoster1Application.java
@@ -0,0 +1,12 @@
+package com.coding.StudentRoster1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRoster1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRoster1Application.class, args);
+	}
+}
